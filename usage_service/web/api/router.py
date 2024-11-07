3b8269cc41cb933d0e1b53f88da99e93d7d9fd5a
Alan Donohoe <alandonohoe@hotmail.com>
2024-11-07 07:25:00 +0000

    refactor: move usage router into its own routers dir

--- usage_service/web/api/router.py
+++ usage_service/web/api/router.py
@@ -1,8 +1,11 @@
+# Main router that includes all other routers.
+
 from fastapi.routing import APIRouter
 
 from usage_service.web.api import monitoring
-from usage_service.web.api.v1 import usage
+from usage_service.web.api.v1.routers import usage
 
 api_router = APIRouter()
+
 api_router.include_router(monitoring.router)
 api_router.include_router(usage.router)
